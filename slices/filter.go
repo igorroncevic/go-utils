34cfe8dc0ca8631db3705effe403be9c6a94da4b
igorroncevic <ironcevic98@gmail.com>
2023-06-28 14:16:30 +0200

    chore: added README.md

--- slices/filter.go
+++ slices/filter.go
@@ -3,10 +3,10 @@ package slices
 // Filter filters out elements that do not satisfy the requirement specified in the filtrator func and returns the filtrated slice.
 //
 //	allPositive := []int{1, 2, 3}
-//	result := Every(allPositive, func(elem int){ return elem > 0 }) // returns [1, 2, 3]
+//	result := Filter(allPositive, func(elem int){ return elem > 0 }) // returns [1, 2, 3]
 //
 //	allPositiveButOne := []int{-10, 6, 14}
-//	result := Every(allPositiveButOne, func(elem int){ return elem > 0 }) // returns [6, 14]
+//	result := Filter(allPositiveButOne, func(elem int){ return elem > 0 }) // returns [6, 14]
 func Filter[T any](slice []T, filtrator func(T) bool) []T {
 	var res []T
 
